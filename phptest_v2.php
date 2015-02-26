d5dec8214885a7db482becd9c85159f182721550
haskelda <haskelda@onid.oregonstate.edu>
2015-02-26 14:17:19 -0800

    added php test

---
+++ phptest_v2.php
@@ -0,0 +1,35 @@
+
+
+    <HTML>
+    <HEAD>
+    <TITLE>My first PHP script</TITLE>
+    </HEAD>
+    <BODY>
+
+
+
+    <?php
+
+
+
+    /* the above "<?php" signals that the PHP script has begun */
+
+    $today = date("Y-m-d");
+
+
+
+    PRINT "<CENTER>Today is: $today.</CENTER>";
+
+
+
+    /* the following "?>" closes the script */
+
+
+
+    ?>
+
+
+
+    </BODY>
+    </HTML>
+
